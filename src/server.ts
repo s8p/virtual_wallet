3c232e1d7d81b36f21000f1613d29c2b6296a028
Rodrigo de Souza <souzabpr@tuta.io>
2022-06-30 16:55:37 -0300

    feat: better feedback on initialization

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import app from './app'
 import AppDataSource from './data-source'
+import dotenv from 'dotenv'
+dotenv.config()
+
 AppDataSource.initialize()
   .then(() => {
-    console.log('Database Connected')
+    console.log('💾 Database Connected')
     const port = process.env.PORT || 3000
 
     app.listen(port, () => {
-      console.log(`🚀 App running on port ${port}`)
+      console.log(
+        `🚀 App running on:\n>\t${
+          process.env.API_URL || 'http://localhost:' + port
+        }`
+      )
     })
   })
   .catch((e) => console.error(e))
